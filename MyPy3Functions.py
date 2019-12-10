03c57bdc5100f1c9db1bfcbb41238ff08a212119
Marc Kiely <45923115+MemeScribe@users.noreply.github.com>
2019-12-10 12:45:56 +0000

    Add files via upload

--- MyPy3Functions.py
+++ MyPy3Functions.py
@@ -1,11 +1,22 @@
 # !/usr/bin/python3
 import os, pygame
-#plays mp3 file
+import sqlite3
+import wave
+from tkinter import StringVar
+from sqlite3 import Error
+from mutagen.mp3 import MP3
+
+path = os.path.expanduser('~/Music/')
+db = r"songs.db"
+song_ended = pygame.USEREVENT + 1
+#Class for handling Play menu functions
 class Play(object):
     def __init__(self):
         self.paused = False
         self.counter = 0
         self.volume = 0.5
+        self.length = StringVar()
+        self.length.set("0:00")
 
     def toggle(self):
         if self.paused:
@@ -15,61 +26,174 @@ class Play(object):
             pygame.mixer.music.pause()
             self.paused = True
 
-    def prev(self, dic):
-        if self.counter == 0:
-            self.counter = len(dic) - 1
-        else:
-            self.counter -= 1
-        prev = list(dic.keys())[self.counter]
-        print(prev, self.counter)
-        pygame.mixer.music.load("songs/"+prev)
+    def prev(self):
+        conn = create_connection(db)
+        self.counter -= 1
+        try:
+            prev,ftype = select_songid(conn, self.counter)[0][:2]
+        except IndexError:
+            prev,ftype = select_songname(conn, "*")[-1][:2]
+            self.counter = int(select_songname(conn, "*")[-1][3])
+        print(prev, length(prev+"."+ftype) ,self.counter)
+        pygame.mixer.music.load(path+prev+"."+ftype)
         pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play()
         self.paused = False
+        self.printlen()
+        pygame.mixer.music.set_endevent(song_ended)
 
-    def next(self, dic):
-        if self.counter == len(dic) - 1:
+    def next(self):
+        conn = create_connection(db)
+        self.counter += 1
+        try:
+            skip,ftype = select_songid(conn, self.counter)[0][:2]
+        except IndexError:
+            skip,ftype = select_songid(conn, 0)[0][:2]
             self.counter = 0
-        else:
-            self.counter += 1
-        prev = list(dic.keys())[self.counter]
-        print(prev, self.counter)
-        pygame.mixer.music.load("songs/"+prev)
+        print(skip, length(skip+"."+ftype) ,self.counter)
+        pygame.mixer.music.load(path+skip+"."+ftype)
         pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play()
         self.paused = False
+        self.printlen()
+        pygame.mixer.music.set_endevent(song_ended)
 
-    def StartSong(self, song, dic):
-        pygame.mixer.music.load("songs/"+song)
+    def StartSong(self, song, songid):
+        self.counter = int(songid)
+        print(song, length(song), self.counter)
+        pygame.mixer.music.load(path+song)
         pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play()
-        self.counter = dic[song]
+        pygame.mixer.music.set_endevent(song_ended)
         self.paused = False
+        self.printlen()
+
+    def printlen(self):
+        conn = create_connection(db)
+        curr_len = select_songid(conn, self.counter)[0][2]
+        self.length.set(curr_len)
 
     def volume_slider(self, n):
         self.volume = int(n) / 100
         pygame.mixer.music.set_volume(self.volume)
 
+    def music_slider(self, n):
+        conn = create_connection(db)
+        file = select_songid(conn, self.counter)[0]
+        try:
+            length = min_to_sec(file[2])
+            n = (int(n) / 100) * length
+            pygame.mixer.music.rewind()
+            pygame.mixer.music.set_pos(float(n))
+        except pygame.error:
+            # if file[1] == "wav":
+            #     with wave.open(file[0]+".wav") as file:
+            #         rate = file.getframerate()
+            #         length = file.getnframes()
+            # else:
+            print("Mypy3 doesn't support filetype:", ftype)
 
-def add_new(): #scans songs.csv and checks if any songs not already in playlist
-    l=set()
-    if "songs.txt" not in os.listdir():
-        open("songs.txt", "a+").close()
-    for file in os.listdir("songs"):
-        with open("songs.txt", "r+") as f:
-            if file+'\n' not in f.readlines():
-                l.add(file)
-    with open("songs.txt", "a") as f:
-        l = sorted(list(l))
-        for file in l:
-            f.write(file+"\n")
 
-def FullList(): #Opens songs.csv and returns them as a numbered dictionary
-    i = 0
-    d = {}
-    with open("songs.txt","r") as s:
-        for f in s.readlines():
-            f = f.strip()
-            d[f] = i
-            i += 1
-    return d
+# Functions for handling SQLite3 with songs
+
+def create_connection(db_file):
+# create a database connection to a SQLite database
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+def create_table(conn, create_table_sql):
+    # create a table from the create_table_sql statement
+    # :param conn: Connection object
+    # :param create_table_sql: a CREATE TABLE statement
+    # :return:
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def insert_song(conn, song):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    try:
+        sql = ''' INSERT INTO songs(name,length,filetype)
+                  VALUES(?,?,?) '''
+        cur = conn.cursor()
+        cur.execute(sql, song)
+        return cur.lastrowid
+    except Error:
+        pass
+
+def update_id(conn, song):
+    """
+    update priority, begin_date, and end date of a task
+    :param conn:
+    :param task:
+    :return: project id
+    """
+    sql = ''' UPDATE songs
+              SET id = ?
+              WHERE name = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, song)
+    conn.commit()
+
+def select_songname(conn, song):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    if song == "*":
+        cur.execute("SELECT name, filetype, length, id FROM songs ORDER BY id")
+    elif song == "__ALPHA__":
+        cur.execute("SELECT name, filetype, length, id FROM songs ORDER BY name")
+    else:
+        cur.execute("SELECT name, filetype, length, id FROM songs WHERE name=?", (song,))
+ 
+    return cur.fetchall()
+
+def select_songid(conn, sid):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT name, filetype, length, id FROM songs WHERE id=?", (sid,))
+ 
+    return cur.fetchall()
+
+# BEGINNING OF NON-SQL FUNCTIONS
+# ================================================================================================================================================================================
+
+def length(song):
+    pygame.mixer.music.load(path+song)
+    if song[-3:] == "mp3":
+        song = MP3(path+song)
+        sec = int(song.info.length)
+        time = str(sec // 60)+":"+str(sec % 60)
+    else:
+        sec = int(pygame.mixer.Sound(path+song).get_length())
+        time = str(sec // 60)+":"+str(sec % 60)
+    if len(time) < 4:
+        time = time[:2] +"0"+ time[-1]
+    return time
+
+def min_to_sec(time):
+    return int(time[0]) * 60 + int(time[-2])
+
+def sec_to_min(time):
+    return str(time // 60) + ":" + str(time % 60)
--- MyPyMain.py
+++ MyPyMain.py
@@ -1,9 +1,14 @@
 # !/usr/bin/python3
 from tkinter import * #Importing Tkinter
+from tkinter import font
 from PIL import Image, ImageTk #Importing Image handling libraries
+import sqlite3
+from sqlite3 import Error
 from MyPy3Functions import Play
 import MyPy3Functions as func
 import os, pygame #Importing os pathfinding, Pygame mixer tools and external functions
+
+path = os.path.expanduser('~/Music/')
 
 # Initialising application window size, background, favicon ect.
 root = Tk()
@@ -11,13 +16,43 @@ root.title('MyPy3')
 root.geometry('1900x1000')
 root.configure(background = '#302f29')
 imgicon = PhotoImage(file=os.path.join('~/MyPy3/ico/logo_ico/','ms-icon-310x310.png'))
-root.tk.call('wm', 'iconphoto', root._w, imgicon)  
+root.tk.call('wm', 'iconphoto', root._w, imgicon)
 
-# scanning for new songs
-func.add_new()
+# Initialising Pygame mixer
+pygame.init()
+pygame.mixer.init()
+obj = Play() #Class object used for music menu functions
+song_ended = pygame.USEREVENT + 1
 
-# Assigning songs to a dictionary for labeling
-song_list = func.FullList()
+# Initialising database songs list
+db = r"songs.db"
+sql_create_songs_table = """ CREATE TABLE IF NOT EXISTS songs (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL UNIQUE,
+                                    length text NOT NULL,
+                                    filetype text NOT NULL
+                                ); """
+conn = func.create_connection(db)
+if conn is not None:
+    func.create_table(conn, sql_create_songs_table)
+else:
+    print("Error: Database could not be created.")
+
+names = []
+for x in func.select_songname(conn, "*"):
+    names.append(x[0])
+for files in sorted(os.listdir(path)):
+    if files[:-4] not in names:
+        pygame.mixer.music.load(path+files)
+        length = func.length(files)
+        song = (files[:-4],length,files[-3:])
+        with conn:
+            func.insert_song(conn,song)
+try:
+    length = length
+except NameError:
+    for i in range(int(func.select_songname(conn, "*")[-1][-1])):
+        func.update_id(conn, (i, func.select_songname(conn, "__ALPHA__")[i][0]))
 
 # placing Album image above songs list
 load = Image.open("Album.jpg")
@@ -27,30 +62,46 @@ img = Label(root, image=render)
 img.image = render
 img.grid(row = 0, column = 0)
 
-# Initialising Pygame mixer
-pygame.init()
-pygame.mixer.init()
-obj = Play() #Class object used for music menu functions
 
 # Creating song list using a for loop to generate buttons based on the number of songs in 'song_list' dict
 btn_dic = {}
-row = 7
-for k in song_list:
-    action = lambda x = k: obj.StartSong(x, song_list)
-    btn_dic[k] = Button(root, text = k[:-4], width="30", anchor="w",
-     command = action)
-    btn_dic[k].grid(row = row, column = 0, pady = 5)
+
+canvas = Canvas(root, borderwidth = 0,background="#302f29", width=270, height=750)
+
+frame = LabelFrame(canvas, bg = '#302f29')
+frame.grid(row=7,column=0)
+
+scroll = Scrollbar(root, orient="vertical",command=canvas.yview)
+scroll.grid(row=7,column=1,sticky="ns")
+
+canvas.configure(yscrollcommand = scroll.set)
+canvas.grid(row=7,column=0)
+canvas.create_window((0,0), window=frame, anchor="nw")
+scroll.config(command=canvas.yview)
+
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
+frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+
+row=7
+for x in func.select_songname(conn, "__ALPHA__"):
+    action = lambda x = x: obj.StartSong(x[0]+"."+x[1],x[3])
+    btn_dic[x[0]] = Button(frame, text = x[0], width="30", anchor="w",
+     cursor="hand2", command = action)
+    btn_dic[x[0]].grid(row=row, pady=1)
     row += 1
 #creating Play/Pause, next and prev buttons
+curr_length = obj.length
 
 play_pause = Button(root, text = "Play/Pause", command = obj.toggle)
 play_pause.place(x=900, y=900)
 
-action = lambda x=song_list: obj.prev(x)
+action = lambda : obj.prev()
 prev = Button(root, text = "Prev", command = action)
 prev.place(x=825, y=900)
 
-action = lambda x=song_list: obj.next(x)
+action = lambda : obj.next()
 skip = Button(root, text = "Next", command = action)
 skip.place(x=1020, y=900)
 
@@ -61,4 +112,22 @@ volume = Scale(root, variable=var, orient=HORIZONTAL, length=200, command = acti
 volume.place(x=1100, y=900)
 volume.set(50)
 
-root.mainloop()
+var2 = DoubleVar()
+action = lambda x=var2: obj.music_slider(x)
+pos = Scale(root, variable=var2, orient=HORIZONTAL, length=1200, showvalue=0, command = action)
+pos.place(x=400, y=850)
+
+length_full = Message(root, textvariable=obj.length, width=30)
+length_full.place(x=1610,y=860)
+curr = "0:00"
+length_current = Message(root, textvariable=curr, width=30)
+length_current.place(x=390, y=860)
+
+while True:
+    var2.set((pygame.mixer.music.get_pos() / 1000) / func.min_to_sec(func.select_songid(conn, obj.counter)[0][2]) * 100)
+    curr = func.sec_to_min((pygame.mixer.music.get_pos() / 1000) / func.min_to_sec(func.select_songid(conn, obj.counter)[0][2]) * 100)
+    for event in pygame.event.get():
+        if event.type == song_ended:
+            obj.next()
+    root.update()
+# root.mainloop()
